03e81bd19865068bc9b60e00f47c5d9b395d7d7f
Michael Chen <miketychen@gmail.com>
2017-08-19 11:42:40 -0700

    initial setup for portfolio framework

---
+++ app/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Michael Chen's Portfolio Site</h3>
+      </div>
+    );
+  }
+};
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+var PORT = process.env.PORT || '3030';
+
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  },
+  devServer: {
+    port: PORT
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin({
+        filename: 'css/main.css',
+        allChunks: true
+    })
+  ]
+};
